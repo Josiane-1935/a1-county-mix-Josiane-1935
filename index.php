20afd4122938f85381a2c2ca003636fc4d1bd126
MangoJosiane <mango.heri@strathmore.edu>
2020-05-26 09:34:18 +0300

    added index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require_once 'county_mix.php'; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>a1-county-mix-Josiane-1935</title>
+  </head>
+  <body>
+    
+  </body>
+</html>
